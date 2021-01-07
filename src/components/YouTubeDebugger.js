85560343be3973146706f7dbcff1d75af80c36e6
Jana Zrayka <janjoune.97@hotmail.com>
2021-01-07 22:10:09 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -5,17 +5,40 @@ export default class YouTubeDebugger extends Component{
   constructor(){
     super();
     
+    this.state = {
+  errors: [],
+  user: null,
+  settings: {
+  bitrate: 8,
+  video: {
+  resolution: '1080p'
+    }
+  }
+}
+    
   }
   
    handleClick = () => {
     this.setState ({
-      
+      settings:{
+        bitrate:12
+      }
+    })
+  }
+  
+  handleClick2 = () => {
+    this.setState ({
+      settings:{
+        video:{
+          resolution:'720p'
+        }
+      }
     })
   }
   
   render () {
     return (
-      <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+      <button className="bitrate" onClick={this.handleClick}>click</button>
       )
   }
 }
